9fcf5a65e055a0740b2379bea1b813a032fdd9d8
prof001 <oshalusijohn@gmail.com>
2020-01-31 20:21:17 +0100

    Fresh updates

--- shopping-frontend/src/app/app-routing.module.ts
+++ shopping-frontend/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
+import { ShoppingCartComponent } from "./shopping-cart/shopping-cart.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "shopping-cart",
+    component: ShoppingCartComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
